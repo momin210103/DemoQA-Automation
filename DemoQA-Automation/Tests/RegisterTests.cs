7b9d7b93b9225f9b05a10afaee8baed9045109b0
Momin Sheikh <momincse13@gmail.com>
2026-04-13 13:43:14 +0600

    Add Valid_Register_Test_Method using follow AAA Patern and it working properly

--- DemoQA-Automation/Tests/RegisterTests.cs
+++ DemoQA-Automation/Tests/RegisterTests.cs
@@ -1,6 +1,26 @@
+using DemoQA_Automation.Pages;
+using FluentAssertions;
+using Xunit.Abstractions;
+
 namespace DemoQA_Automation.Tests;
 
-public class RegisterTests
+public class RegisterTests(ITestOutputHelper output) : BaseTest
 {
-    
+    private readonly ITestOutputHelper _output = output;
+
+    [Fact]
+    public void Valid_Register_Test()
+    {
+        // Arrange
+        Driver.Navigate().GoToUrl("https://demoqa.com/register");
+        var registerPage = new RegisterPage(Driver);
+        // Act
+        registerPage.Register_Successfully("Mom-in","sheikh","momin123","momin1234");
+        
+        //Assert
+        var error = registerPage.GetCaptchaError();
+        error.Should().Contain("reCaptcha");
+
+
+    }
 }
