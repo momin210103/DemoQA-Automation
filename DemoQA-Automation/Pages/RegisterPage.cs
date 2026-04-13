e5d1d0f46ececf13e8b60793c0c7f4184b9b3f8c
Momin Sheikh <momincse13@gmail.com>
2026-04-13 12:57:33 +0600

    Set Register Successfully Method

--- DemoQA-Automation/Pages/RegisterPage.cs
+++ DemoQA-Automation/Pages/RegisterPage.cs
@@ -1,6 +1,24 @@
+using OpenQA.Selenium;
+using Xunit.Abstractions;
+
 namespace DemoQA_Automation.Pages;
 
-public class RegisterPage
+public class RegisterPage(IWebDriver driver) : BasePage(driver)
 {
-    
+    private readonly By _firstName = By.Id("firstName");
+    private readonly By _lastName = By.Id("lastName");
+    private readonly By _userName = By.Id("userName");
+    private readonly By _password = By.Id("password");
+    private readonly By _register = By.Id("register");
+
+    public void Register_Successfully(string firstname, string lastname,string username,string password)
+    {
+        Type(_firstName,firstname);
+        Type(_lastName, lastname);
+        Type(_userName, username);
+        Type(_password, password);
+        Click(_register);
+    }
+
+
 }
