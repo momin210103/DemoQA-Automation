34b4b6c264675923bb1124d05babe4f2f4b33fdd
Momin Sheikh <momincse13@gmail.com>
2026-04-13 14:50:44 +0600

    Implement Explicit WebHelper

--- DemoQA-Automation/Utitlities/WaitHelper.cs
+++ DemoQA-Automation/Utitlities/WaitHelper.cs
@@ -1,6 +1,14 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
 namespace DemoQA_Automation.Utitlities;
 
-public class WaitHelper
+public static class WaitHelper
 {
+    public static IWebElement WaitForElement(IWebDriver driver, By by)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        return wait.Until(d => d.FindElement(by));
+    }
     
 }
